2ce3fde28d02a972bf3045e0157bae1920d354e2
HolySephi <me@sephi.de>
2020-04-26 14:26:09 +0200

    updated bootstrap

---
+++ bak/js/fsv.js
@@ -0,0 +1,6 @@
+
+$('.navbar .dropdown').hover(function() {
+  $(this).find('.dropdown-menu').first().stop(true, true).delay(50).slideDown();
+}, function() {
+  $(this).find('.dropdown-menu').first().stop(true, true).delay(10).slideUp()
+});
