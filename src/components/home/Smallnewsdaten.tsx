763907855873a9a001252376fbe80349798bcfd2
Eddi <Eddionline85@gmail.com>
2025-07-06 12:36:02 +0200

    Update Smallnewsdaten.tsx

--- src/components/home/Smallnewsdaten.tsx
+++ src/components/home/Smallnewsdaten.tsx
@@ -2,16 +2,16 @@
   // This file contains the chronik data for the FSV Gunzenhausen website
   
   const smallNewsDaten =  [
+        {
+      title: "Erster Alleinflug Emilio!",
+      date: "15.06.2025",
+      description: 'Unser Mitgleid Emilio hat heute seinen ersten Alleinflug im Segelflug mit unserer ASK21. Wir wünschen ihm viel Erfolg bei der weiteren Ausbildung und viele schöne Flüge!',
+      images: []
+    },
      {
       title: "Die Flugsaison 2025 beginnt!",
       date: "05.04.2025",
       description: 'Endlich ist es soweit – die neue Flugsaison beginnt! Die Flugsportvereinigung Gunzenhausen e.V. startet am Samstag, den 05. April 2025, offiziell in die neue Saison.',
-      images: []
-    },
-    {
-      title: "Flotte vergrößert!",
-      date: "21.01.2025",
-      description: 'Unsere Flotte ist seit dem 21.01.2025 gewachsen! Wir haben nun als Vereinsflugzeug für alle Mitglieder eine Aerospool Dynamic WT9 600 mit dem Kennzeichen D-MFVG zur Verfügung.',
       images: []
     },
   ];
