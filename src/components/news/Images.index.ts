52f55e3a79ad7cf2d8605a6ea82fbaccb8d2eac2
Eddi <Eddionline85@gmail.com>
2025-07-06 12:45:14 +0200

    Update Images.index.ts

--- src/components/news/Images.index.ts
+++ src/components/news/Images.index.ts
@@ -1,8 +1,12 @@
 import pic001 from '../../assets/components/chronik/01-01.jpg';
 import pic001Prev from '../../assets/components/chronik/01-01_prev.jpg';
+import pic002 from '../../assets/components/news/02-02.jpg';
+import pic002Prev from '../../assets/components/news/02-02.jpg';
+
 const imageStore = {
   // Image store with full and preview images}
   pic001: { full: pic001, preview: pic001Prev },
+  pic002: { full: pic002, preview: pic002Prev }
 };
 
 export default imageStore;
