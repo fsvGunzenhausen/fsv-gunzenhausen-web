239301347d41b10862710e77884fb8ecea293d56
cfGauss-dev <eddionline85@gmail.com>
2025-12-26 18:22:49 +0100

    extending 2026 fixes

--- src/components/rundfluege/RundfluegeSubHeader.tsx
+++ src/components/rundfluege/RundfluegeSubHeader.tsx
@@ -4,10 +4,10 @@ function RundfluegeSubHeader() {
 
   return (
     <>
-    <h4 className="text-black-50">
+    <h5 className="text-black-50">
       Gerne fliegen unsere erfahrenen Piloten zu Zielen in der näheren
       oder weiteren Umgebung.
-    </h4>
+    </h5>
   </>
   );
 }
